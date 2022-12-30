a7ee993e674b373209739e938400e2207e9fbcc5
Fabian FHTW <74427715+Battlecoob@users.noreply.github.com>
2022-12-30 13:11:19 +0100

    Update Program.cs

--- wordcount/Program.cs
+++ wordcount/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace WordCount
 {
@@ -9,9 +10,19 @@ namespace WordCount
     {
         // This function is pure because it does not have any side effects and always returns the same
         // result for the same input. It also does not depend on any external state.
-        static IEnumerable<string> GetWords(string text)
+        static IEnumerable<string> GetWordsOther(string text)
         {
             return text.Split(' ');
+        }
+
+        // This function is pure because it does not have any side effects and always returns the same
+        // result for the same input. It also does not depend on any external state.
+        static IEnumerable<string> GetWords(string text)
+        {
+            // Use a regular expression to only include words and numbers in the output
+            Regex regex = new Regex(@"\b[\w\d]+\b");
+
+            return regex.Matches(text).Cast<Match>().Select(match => match.Value);
         }
 
         // This function is pure because it does not have any side effects and always returns the same
@@ -29,15 +40,13 @@ namespace WordCount
         {
             foreach ((string word, int count) in wordCounts.OrderByDescending(pair => pair.Count))
             {
-                Console.WriteLine($"{word}: {count}");
+                Console.WriteLine($"{word} -> {count}");
             }
         }
 
         static void Main(string[] args)
         {
             // Check if there are enough command line arguments
-
-            /*
             if (args.Length < 2)
             {
                 Console.WriteLine("Error: Please provide a directory path and file extension.");
@@ -47,10 +56,6 @@ namespace WordCount
             // Get the directory path and file extension from the command line arguments
             string directoryPath = args[0];
             string fileExtension = args[1];
-            */
-
-            string directoryPath = "/Users/fabian/My Drive/Studium/compSci_Dual/5_Sem/FPROG/Wordcount-Projekt/wordlists";
-            string fileExtension = ".txt";
 
             // Use a higher-order function to enumerate all the files in the directory that match the file extension
             // and read their contents, resulting in a list of lists of words
